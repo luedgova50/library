7cef5802b916f27fdc50262181b2e6036a60a7f6
luedgova <luedgova1967@gmail.com>
2019-09-09 22:10:16 -0500

    Routes Permiso

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,13 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'InicioController@index');
+Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => ['auth', 'superadmin']], function () {
+    Route::get('', 'AdminController@index');
+    /*RUTAS DE PERMISO*/
+    Route::get('permiso', 'PermisoController@index')->name('permiso');
+    Route::get('permiso/create', 'PermisoController@create')->name('create_permiso');
+    Route::post('permiso', 'PermisoController@storage')->name('storage_permiso');
+    Route::get('permiso/{id}/edit', 'PermisoController@edit')->name('edit_permiso');
+    Route::put('permiso/{id}', 'PermisoController@update')->name('update_permiso');
+    Route::delete('permiso/{id}', 'PermisoController@destroy')->name('destroy_permiso');
