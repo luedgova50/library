6876feb89de6cfa0aa8c6d65ec623754634a8e3d
luedgova <luedgova1967@gmail.com>
2019-09-10 10:55:53 -0500

    Menu and MenuRol and Routes Menu

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,12 @@ Route::group(['prefix' => 'admin', 'namespace' => 'Admin'], function () {
    Route::get('permiso/{id}/edit', 'PermisoController@edit')->name('edit_permiso');
    Route::put('permiso/{id}', 'PermisoController@update')->name('update_permiso');
    Route::delete('permiso/{id}', 'PermisoController@destroy')->name('destroy_permiso');
+   /*RUTAS DEL MENU*/
+   Route::get('menu', 'MenuController@index')->name('menu');
+   Route::get('menu/create', 'MenuController@create')->name('create_menu');
+   Route::post('menu', 'MenuController@store')->name('store_menu');
+   Route::get('menu/{id}/edit', 'MenuController@edit')->name('edit_menu');
+   Route::put('menu/{id}', 'MenuController@update')->name('update_menu');
+   Route::get('menu/{id}/detroy', 'MenuController@destroy')->name('detroy_menu');
+   Route::post('menu/guardar-orden', 'MenuController@guardarOrden')->name('store_orden');
 });
