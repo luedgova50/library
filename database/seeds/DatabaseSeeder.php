a501c1f368dc56f6869c481a9d26bd349b2339d5
luedgova <luedgova1967@gmail.com>
2019-09-09 19:45:33 -0500

    Seeders and Factories

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,25 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->truncateTablas([
+            'rol',
+            'permiso',
+            //'usuario',
+            //'usuario_rol'
+        ]);
+
         // $this->call(UsersTableSeeder::class);
+        $this->call(RolTableSeeder::class);
+        $this->call(PermisoTableSeeder::class);
+
+    }
+
+    protected function truncateTablas(array $tablas)
+    {
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0;');
+        foreach ($tablas as $tabla) {
+            DB::table($tabla)->truncate();
+        }
+        DB::statement('SET FOREIGN_KEY_CHECKS = 1;');
     }
 }
---
+++ database/seeds/PermisoTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Models\Permiso;
+use Illuminate\Database\Seeder;
+
+class PermisoTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        factory(Permiso::class)->times(50)->create();
+    }
+}
